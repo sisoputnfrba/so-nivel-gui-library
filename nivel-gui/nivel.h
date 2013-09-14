a9e84757a60c723988feadd297f02c592a583a31
gastonprieto <gaston.prieto.utn@gmail.com>
2013-09-14 11:02:17 -0300

    Uso list en la lib de nivel-gui

--- nivel-gui/nivel.h
+++ nivel-gui/nivel.h
@@ -1,20 +1,26 @@
+#ifndef __NIVEL_H__
+
+#define __NIVEL_H__
+
 #define PERSONAJE_ITEM_TYPE 0
 #define RECURSO_ITEM_TYPE 1
 #define ENEMIGO_ITEM_TYPE 2
 
-struct item {
+#include <commons/collections/list.h>
+
+typedef struct item {
 	char id;
 	int posx;
 	int posy;
 	char item_type; // PERSONAJE o CAJA_DE_RECURSOS
 	int quantity;
 	struct item *next;
-};
-
-typedef struct item ITEM_NIVEL;
+} ITEM_NIVEL;
 
 void rnd(int *x, int max);
-int nivel_gui_dibujar(ITEM_NIVEL* items, char* nom_nivel);
+int nivel_gui_dibujar(t_list* items, char* nombre_nivel);
 int nivel_gui_terminar(void);
 int nivel_gui_inicializar(void);
-int nivel_gui_get_area_nivel(int * rows, int * cols);
+int nivel_gui_get_area_nivel(int * filas, int * columnas);
+
+#endif 
