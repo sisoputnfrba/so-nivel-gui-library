7fc33c6ba2c94e578c0b5dc53181295c5cd309e2
Agustin Ranieri <aguseranieri@gmail.com>
2021-03-22 23:37:42 -0300

    docs: add tad_items documentation

--- nivel-gui/tad_items.h
+++ nivel-gui/tad_items.h
@@ -6,64 +6,97 @@
 
 	/*
 	* @NAME: crear_personaje
-	* @DESC: 
+	* @DESC: Crea una instancia de personaje que se mostrará
+	* en el mapa con el caracter que lo identifica en color 
+	* blanco y fondo en negro.
 	* @PARAMS:
+	*     id - identificador único de acceso
+	*     x  - posición inicial en x
+	*     y  - posición inicial en y
 	*/
 	void crear_personaje(char id, int x , int y);
 	
 	/*
 	* @NAME: crear_enemigo
-	* @DESC: 
+	* @DESC: Crea una instancia de enemigo que se mostrará
+	* en el mapa con el caracter '*' en color negro y fondo
+	* en azul.
 	* @PARAMS:
+	*     id - identificador único de acceso
+	*     x  - posición inicial en x
+	*     y  - posición inicial en y
 	*/
 	void crear_enemigo(char id, int x , int y);
 	
 	/*
 	* @NAME: crear_caja
-	* @DESC: 
+	* @DESC: Crea una instancia de caja que se mostrará en
+	* el mapa con el caracter que lo identifica en color negro
+	* y fondo en amarillo.
 	* @PARAMS:
+	* @PARAMS:
+	*     id   - identificador único de acceso
+	*     x    - posición inicial en x
+	*     y    - posición inicial en y
+	*     cant - cantidad inicial de recursos contenidos en la caja
 	*/
 	void crear_caja(char id, int x , int y, int cant);
 
 	/*
 	* @NAME: borrar_item
-	* @DESC: 
+	* @DESC: Elimina un item que se encuentre en el mapa.
 	* @PARAMS:
+	*     id - identificador único de acceso
 	*/
 	void borrar_item(char id);
 
 	/*
 	* @NAME: mover_item
-	* @DESC: 
+	* @DESC: Mueve el item a la posición indicada. En caso de 
+	* moverse fuera de la pantalla, no realiza ninguna acción.
 	* @PARAMS:
+	*     id - identificador único de acceso
+	*     x  - nueva posición en x
+	*     y  - nueva posición en y
 	*/
 	void mover_item(char id, int x, int y);
 
 	/*
 	* @NAME: desplazar_item
-	* @DESC: 
+	* @DESC: Desplaza el item tomando a su posición actual
+	* como pivote. En caso de desplazarse fuera de la pantalla, 
+	* no realiza ninguna acción.
 	* @PARAMS:
+	*     id - identificador único de acceso
+	*     x  - desplazamiento en x
+	*     y  - desplazamiento en y
 	*/
 	void desplazar_item(char id, int x, int y);
 
 	/*
 	* @NAME: restar_recurso
-	* @DESC: 
+	* @DESC: Quita un recurso de la caja. Si la caja está
+	* vacía, no realiza ninguna acción.
 	* @PARAMS:
+	*     id - identificador único de acceso
 	*/
 	void restar_recurso(char id);
 	
 	/*
 	* @NAME: sumar_recurso
-	* @DESC: 
+	* @DESC: Agrega un recurso a la caja.
 	* @PARAMS:
+	*     id - identificador único de acceso
 	*/
 	void sumar_recurso(char id);
 
 	/*
 	* @NAME: items_chocan
-	* @DESC: 
+	* @DESC: Devuelve true si ambos items se encuentran
+	* en la misma posición
 	* @PARAMS:
+	*     id1 - identificador único de acceso del ítem 1
+	*     id2 - identificador único de acceso del ítem 2
 	*/
 	bool items_chocan(char id1, char id2);
 
